282c4119ccb089a278e923be6186b0aa178cafa7
jaskang <jaskang@wesure.cn>
2022-01-21 14:41:12 +0800

    chore: add lcov

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
     setupFiles: ['vitest.setup.ts'],
     coverage: {
       include: ['packages/*/src/**/*.ts'],
+      reporter: ['text', 'lcov'],
     },
   },
 })
